9a0a733e0246051438ea9cae3d17b522fdbb8e78
MV88 <matteo.velludini@gmail.com>
2022-12-19 23:36:15 +0100

    fix: #4 custom sort import preserving comments

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 import * as vscode from "vscode";
 
 import { sortImports } from "./utils/sortImports";
+import { sortImportsPreservingComments } from "./utils/sortImportsPreservingComments";
 
 export function activate(context: vscode.ExtensionContext) {
   vscode.commands.registerCommand("customImportSort.sortImports", () => {
@@ -13,6 +14,35 @@ export function activate(context: vscode.ExtensionContext) {
         if (/^[jt]sx?$/.test(extension)) {
           const text = document.getText();
           const sortedText = sortImports(text);
+          if (text !== sortedText) {
+            editor.edit((editBuilder) => {
+              editBuilder.replace(
+                new vscode.Range(
+                  new vscode.Position(0, 0),
+                  new vscode.Position(document.lineCount, 0)
+                ),
+                sortedText
+              );
+            });
+          }
+        }
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        vscode.window.showErrorMessage(error.message);
+      }
+    }
+  });
+  vscode.commands.registerCommand("customImportSort.sortImportsPreservingComments", () => {
+    try {
+      const editor = vscode.window.activeTextEditor;
+      if (editor) {
+        const document = editor.document;
+        const fileName = document.fileName;
+        const extension = fileName.substring(fileName.lastIndexOf(".") + 1);
+        if (/^[jt]sx?$/.test(extension)) {
+          const text = document.getText();
+          const sortedText = sortImportsPreservingComments(text);
           if (text !== sortedText) {
             editor.edit((editBuilder) => {
               editBuilder.replace(
